b77f66e7e9aab1b3f23c84ab5258920042e124e9
Tobias Richter <tr4711@gmail.com>
2017-11-08 15:57:26 +0000

    Return liked names on request

--- nameserver.go
+++ nameserver.go
@@ -7,15 +7,25 @@ import (
 	"time"
 
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	rules := getRules()
-	fmt.Fprintf(w, "%v", rules)
+	name := r.URL.Path[1:]
+	if len(name) > 0 {
+		rules := getRules(name)
+		likes := make([][]string, len(rules))
+		for i, rule := range rules {
+			likes[i] = rule.B
+		}
+		fmt.Fprintf(w, "People who liked %s, also liked %s", name, likes)
+	} else {
+		fmt.Fprint(w, getRules(""))
+	}
 	fmt.Println("Request at:", time.Now().Format("2006-01-02 15:04:05"))
 }
 
-func getRules() []AssociationRule {
+func getRules(itemset string) []AssociationRule {
 	session, err := mgo.Dial("127.0.0.1:27017")
 	if err != nil {
 		panic(err)
@@ -25,7 +35,11 @@ func getRules() []AssociationRule {
 	c := session.DB("namefinder").C("rules")
 
 	results := make([]AssociationRule, 1)
-	err = c.Find(nil).All(&results)
+	if len(itemset) > 0 {
+		err = c.Find(bson.M{"a": itemset}).All(&results)
+	} else {
+		err = c.Find(nil).All(&results)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
