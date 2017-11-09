3bfe3c8baa40ed1b8136bc449f5909be8fe59687
Tobias Richter <tr4711@gmail.com>
2017-11-09 16:36:24 +0000

    Refactor Nameserver for clarity

--- nameserver.go
+++ nameserver.go
@@ -7,7 +7,7 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-func GetRules(itemset string) []AssociationRule {
+func GetRules(itemset string) AssociationRules {
 	session, err := mgo.Dial("127.0.0.1:27017")
 	if err != nil {
 		panic(err)
@@ -18,12 +18,15 @@ func GetRules(itemset string) []AssociationRule {
 
 	c := session.DB("namefinder").C("rules")
 
-	results := make([]AssociationRule, 1)
-	if len(itemset) > 0 {
-		err = c.Find(bson.M{"a": itemset}).All(&results)
-	} else {
-		err = c.Find(nil).All(&results)
+	var results AssociationRules
+	query := bson.M{"a": itemset}
+
+	// TODO query = nil return all rules
+	if itemset == "" {
+		query = nil
 	}
+
+	err = c.Find(query).All(&results)
 	if err != nil {
 		log.Fatal(err)
 	}
