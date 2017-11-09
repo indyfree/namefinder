0cc25a4dddaf387c85a11e3c808e60fb31e28f17
Tobias Richter <tr4711@gmail.com>
2017-11-09 16:36:05 +0000

    Add json annotation to AssocianRules

--- associationrules.go
+++ associationrules.go
@@ -4,12 +4,15 @@ import "fmt"
 
 // Association Rule in the form {A => B}, where A and B can be itemsets
 type AssociationRule struct {
-	A          []string
-	B          []string
-	Support    int
-	Confidence float64
-	Lift       float64
+	A          []string `json:"a"`
+	B          []string `json:"b"`
+	Support    int      `json:"support"`
+	Confidence float64  `json:"confidence"`
+	Lift       float64  `json:"lift"`
 }
+
+// TODO explain why to declare
+type AssociationRules []AssociationRule
 
 func (a AssociationRule) String() string {
 	return fmt.Sprintf("%s => %s, sup: %d, conf: %f, lift: %f", a.A, a.B, a.Support, a.Confidence, a.Lift)
