2e4ee33881b1f810308cd3aa0fce678a5f142596
Tobias Richter <tr4711@gmail.com>
2017-11-09 18:01:42 +0000

    Refactored rulesserver into own package

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/indyfree/namefinder/ruleserver"
 )
 
 func main() {
-	router := NewRouter()
+	router := ruleserver.NewRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
