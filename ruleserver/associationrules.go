a8aafb23fccad0a82f0787e98f0b2d5df52ba537
Tobias Richter <tr4711@gmail.com>
2017-11-10 11:28:01 +0000

    Change support field  to float64

--- ruleserver/associationrules.go
+++ ruleserver/associationrules.go
@@ -6,7 +6,7 @@ import "fmt"
 type AssociationRule struct {
 	A          []string `json:"a"`
 	B          []string `json:"b"`
-	Support    int      `json:"support"`
+	Support    float64  `json:"support"`
 	Confidence float64  `json:"confidence"`
 	Lift       float64  `json:"lift"`
 }
@@ -15,5 +15,5 @@ type AssociationRule struct {
 type AssociationRules []AssociationRule
 
 func (a AssociationRule) String() string {
-	return fmt.Sprintf("%s => %s, sup: %d, conf: %f, lift: %f", a.A, a.B, a.Support, a.Confidence, a.Lift)
+	return fmt.Sprintf("%s => %s, sup: %f, conf: %f, lift: %f", a.A, a.B, a.Support, a.Confidence, a.Lift)
 }
