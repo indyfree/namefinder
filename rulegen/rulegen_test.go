4ded453ab3d6577f4d812fa9646ec1d801d3f102
Tobias Richter <tr4711@gmail.com>
2017-11-10 11:11:10 +0000

    Test transaction generator

---
+++ rulegen/rulegen_test.go
@@ -0,0 +1,27 @@
+package rulegen
+
+import "testing"
+import "github.com/indyfree/namefinder/ruleserver"
+
+type testpair struct {
+	transactions [][]string
+	min_sup      float64
+	min_conf     float64
+	rules        ruleserver.AssociationRules
+}
+
+var cases = []testpair{
+	{[][]string{{"A", "B"}, {"B", "C"}, {"A", "B", "C"}, {"A", "B"}}, 0.5, 0.5,
+		ruleserver.AssociationRules{ruleserver.AssociationRule{[]string{"A"}, []string{"B"}, 1, 0.75, 0.75}}},
+}
+
+func TestGenerateTransActions(t *testing.T) {
+	itemset := []string{"A", "B", "C", "D"}
+	tnumber := 100
+	transactions := GenerateTransactions(tnumber, itemset)
+
+	if len(transactions) != tnumber {
+		t.Errorf("Number of generated transaction does not match, want: %d, got: %d",
+			tnumber, len(transactions))
+	}
+}
